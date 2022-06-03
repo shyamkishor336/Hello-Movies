9d884617babf37b9e6fed7a22e77fb68d3c5f649
Pandit SK <60429923+shyamkishor336@users.noreply.github.com>
2022-06-03 17:44:59 +0545

    splash

--- app/src/main/java/sk/hello/movies/SplashActivity.java
+++ app/src/main/java/sk/hello/movies/SplashActivity.java
@@ -1,8 +1,14 @@
 package sk.hello.movies;
 
+import static java.lang.Thread.sleep;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +16,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView image = findViewById(R.id.imageView);
+        Glide.with(this).load(R.mipmap.logo).into(image);
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });thread.start();
     }
 }
